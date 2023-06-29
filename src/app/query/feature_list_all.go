7b29760756058e8fd4210675653cf4835d7f25b0
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-29 22:55:32 +0300

    cqrs: complete the feature list all query

--- src/app/query/feature_list_all.go
+++ src/app/query/feature_list_all.go
@@ -2,7 +2,6 @@ package query
 
 import (
 	"context"
-	"fmt"
 
 	"api.turistikrota.com/place/src/domain/feature"
 	"github.com/mixarchitecture/cache"
@@ -11,12 +10,14 @@ import (
 )
 
 type (
-	FeatureListAllQuery   struct{}
-	FeatureListAllResult  struct{}
+	FeatureListAllQuery  struct{}
+	FeatureListAllResult struct {
+		Features []*feature.Entity
+	}
 	FeatureListAllHandler decorator.QueryHandler[FeatureListAllQuery, *FeatureListAllResult]
 	featureListAllHandler struct {
 		repo  feature.Repository
-		cache cache.Client[*feature.Entity]
+		cache cache.Client[[]*feature.Entity]
 	}
 	FeatureListAllHandlerConfig struct {
 		Repo     feature.Repository
@@ -29,32 +30,29 @@ func NewFeatureListAllHandler(config FeatureListAllHandlerConfig) FeatureListAll
 	return decorator.ApplyQueryDecorators[FeatureListAllQuery, *FeatureListAllResult](
 		featureListAllHandler{
 			repo:  config.Repo,
-			cache: cache.New[*feature.Entity](config.CacheSrv),
+			cache: cache.New[[]*feature.Entity](config.CacheSrv),
 		},
 		config.CqrsBase,
 	)
 }
 
 func (h featureListAllHandler) Handle(ctx context.Context, query FeatureListAllQuery) (*FeatureListAllResult, *i18np.Error) {
-	/*
-		    cacheHandler := func() (*post.Entity, *i18np.Error) {
-				return h.repo.ListAll(ctx, post.I18nDetail{
-					Locale: query.Locale,
-					Slug:   query.Slug,
-				})
-			}
-			res, err := h.cache.Creator(h.createCacheEntity).Handler(cacheHandler).Get(h.generateCacheKey(query))
-		    if err != nil {
-				return nil, err
-			}
-	*/
-	return &FeatureListAllResult{}, nil
+	cacheHandler := func() ([]*feature.Entity, *i18np.Error) {
+		return h.repo.ListAll(ctx)
+	}
+	res, err := h.cache.Creator(h.createCacheEntity).Handler(cacheHandler).Get(ctx, h.generateCacheKey(query))
+	if err != nil {
+		return nil, err
+	}
+	return &FeatureListAllResult{
+		Features: res,
+	}, nil
 }
 
-func (h featureListAllHandler) createCacheEntity() *feature.Entity {
-	return &feature.Entity{}
+func (h featureListAllHandler) createCacheEntity() []*feature.Entity {
+	return make([]*feature.Entity, 0)
 }
 
 func (h featureListAllHandler) generateCacheKey(query FeatureListAllQuery) string {
-	return fmt.Sprintf("cache_key")
+	return "feature_list_all"
 }
