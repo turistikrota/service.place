4913975e137e6bc49cf9be918cdb61f4bde0e92e
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-29 23:46:02 +0300

    dto: complete the request interface

--- src/delivery/http/dto/req/req.go
+++ src/delivery/http/dto/req/req.go
@@ -1,9 +1,50 @@
 package req
 
-type Request interface{}
+type Request interface {
+	FeatureDetail() *FeatureDetailRequest
+	Pagination() *PaginationRequest
+	FeatureCreate() *FeatureCreateRequest
+	FeatureUpdate() *FeatureUpdateRequest
+	PlaceDetail() *PlaceDetailRequest
+	PlaceFilter() *PlaceFilterRequest
+	PlaceCreate() *PlaceCreateRequest
+	PlaceUpdate() *PlaceUpdateRequest
+}
 
 type request struct{}
 
 func New() Request {
 	return &request{}
+}
+
+func (r *request) FeatureDetail() *FeatureDetailRequest {
+	return &FeatureDetailRequest{}
+}
+
+func (r *request) Pagination() *PaginationRequest {
+	return &PaginationRequest{}
+}
+
+func (r *request) FeatureCreate() *FeatureCreateRequest {
+	return &FeatureCreateRequest{}
+}
+
+func (r *request) FeatureUpdate() *FeatureUpdateRequest {
+	return &FeatureUpdateRequest{}
+}
+
+func (r *request) PlaceDetail() *PlaceDetailRequest {
+	return &PlaceDetailRequest{}
+}
+
+func (r *request) PlaceFilter() *PlaceFilterRequest {
+	return &PlaceFilterRequest{}
+}
+
+func (r *request) PlaceCreate() *PlaceCreateRequest {
+	return &PlaceCreateRequest{}
+}
+
+func (r *request) PlaceUpdate() *PlaceUpdateRequest {
+	return &PlaceUpdateRequest{}
 }
