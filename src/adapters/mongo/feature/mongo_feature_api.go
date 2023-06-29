52b040c104b7426d2d201fb32b91f31a67e6b170
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-29 20:33:35 +0300

    adapter: add feature mongo repo for first time

--- src/adapters/mongo/feature/mongo_feature_api.go
+++ src/adapters/mongo/feature/mongo_feature_api.go
@@ -1,1 +1,36 @@
 package feature
+
+import (
+	"context"
+
+	"api.turistikrota.com/place/src/domain/feature"
+	"github.com/mixarchitecture/i18np"
+)
+
+func (r *repo) Create(ctx context.Context, entity *feature.Entity) *i18np.Error {
+	return nil
+}
+
+func (r *repo) Update(ctx context.Context, uuid string, entity *feature.Entity) *i18np.Error {
+	return nil
+}
+
+func (r *repo) Disable(ctx context.Context, uuid string) *i18np.Error {
+	return nil
+}
+
+func (r *repo) Enable(ctx context.Context, uuid string) *i18np.Error {
+	return nil
+}
+
+func (r *repo) Delete(ctx context.Context, uuid string) *i18np.Error {
+	return nil
+}
+
+func (r *repo) ListAll(ctx context.Context) ([]*feature.Entity, *i18np.Error) {
+	return nil, nil
+}
+
+func (r *repo) GetByUUIDs(ctx context.Context, uuids []string) ([]*feature.Entity, *i18np.Error) {
+	return nil, nil
+}
--- src/adapters/mongo/feature/mongo_feature_repo.go
+++ src/adapters/mongo/feature/mongo_feature_repo.go
@@ -1,1 +1,36 @@
 package feature
+
+import (
+	"api.turistikrota.com/place/src/adapters/mongo/feature/entity"
+	"api.turistikrota.com/place/src/domain/feature"
+	mongo2 "github.com/turistikrota/service.shared/db/mongo"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+type repo struct {
+	factory    feature.Factory
+	collection *mongo.Collection
+	helper     mongo2.Helper[entity.MongoFeature, *feature.Entity]
+}
+
+func New(factory feature.Factory, collection *mongo.Collection) feature.Repository {
+	validate(factory, collection)
+	return &repo{
+		factory:    factory,
+		collection: collection,
+		helper:     mongo2.NewHelper[entity.MongoFeature, *feature.Entity](collection, createEntity),
+	}
+}
+
+func createEntity() *entity.MongoFeature {
+	return &entity.MongoFeature{}
+}
+
+func validate(factory feature.Factory, collection *mongo.Collection) {
+	if factory.IsZero() {
+		panic("categoryFactory is zero")
+	}
+	if collection == nil {
+		panic("collection is nil")
+	}
+}
