12e34b0d47cfab7fcbde77acae97c8c6595ba251
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-29 22:27:44 +0300

    cqrs: update the cqrs type

--- src/app/app.go
+++ src/app/app.go
@@ -1,10 +1,30 @@
 package app
 
+import (
+	"api.turistikrota.com/place/src/app/command"
+	"api.turistikrota.com/place/src/app/query"
+)
+
 type Application struct {
 	Commands Commands
 	Queries  Queries
 }
 
-type Commands struct{}
+type Commands struct {
+	FeatureCreate  command.FeatureCreateHandler
+	FeatureUpdate  command.FeatureUpdateHandler
+	FeatureDelete  command.FeatureDeleteHandler
+	FeatureDisable command.FeatureDisableHandler
+	FeatureEnable  command.FeatureEnableHandler
+	PlaceCreate    command.PlaceCreateHandler
+	PlaceUpdate    command.PlaceUpdateHandler
+	PlaceDelete    command.PlaceDeleteHandler
+	PlaceDisable   command.PlaceDisableHandler
+	PlaceEnable    command.PlaceEnableHandler
+}
 
-type Queries struct{}
+type Queries struct {
+	FeatureListAll query.FeatureListAllHandler
+	PlaceFilter    query.PlaceFilterHandler
+	PlaceView      query.PlaceViewHandler
+}
