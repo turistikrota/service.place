553b217be486e8a3f8074319de6ce8740b9b3c71
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-30 11:14:48 +0300

    dto: add feature list response dto

--- src/delivery/http/dto/res/feature_list_res.go
+++ src/delivery/http/dto/res/feature_list_res.go
@@ -1,1 +1,24 @@
 package res
+
+import (
+	"api.turistikrota.com/place/src/app/query"
+	"api.turistikrota.com/place/src/domain/feature"
+)
+
+type FeatureListResponse struct {
+	UUID         string                                  `json:"uuid"`
+	Icon         string                                  `json:"icon"`
+	Translations map[feature.Locale]feature.Translations `json:"translations"`
+}
+
+func (r *response) FeatureList(res *query.FeatureListAllResult) []*FeatureListResponse {
+	list := make([]*FeatureListResponse, len(res.Features))
+	for i, v := range res.Features {
+		list[i] = &FeatureListResponse{
+			UUID:         v.UUID,
+			Icon:         v.Icon,
+			Translations: v.Translations,
+		}
+	}
+	return list
+}
