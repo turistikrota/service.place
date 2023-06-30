80b0bc0113204590cb5824b0e5ee5626df67559f
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-30 11:27:55 +0300

    http: add the all handlers for first time

--- src/delivery/http/http_api.go
+++ src/delivery/http/http_api.go
@@ -1,1 +1,55 @@
 package http
+
+import "github.com/gofiber/fiber/v2"
+
+func (h Server) FeatureCreate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) FeatureUpdate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) FeatureDelete(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) FeatureDisable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) FeatureEnable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) FeatureList(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceCreate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceUpdate(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceDelete(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceDisable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceEnable(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceFilter(ctx *fiber.Ctx) error {
+	return nil
+}
+
+func (h Server) PlaceView(ctx *fiber.Ctx) error {
+	return nil
+}
