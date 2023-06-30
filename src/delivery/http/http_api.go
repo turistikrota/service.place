809c41de5e4c7c22cea79f3a52e4c638a55a09c6
Sami Salih İbrahimbaş <info@ssibrahimbas.com>
2023-06-30 11:33:13 +0300

    http: complete the feature http handlers

--- src/delivery/http/http_api.go
+++ src/delivery/http/http_api.go
@@ -1,6 +1,7 @@
 package http
 
 import (
+	"api.turistikrota.com/place/src/app/query"
 	"api.turistikrota.com/place/src/delivery/http/dto"
 	"github.com/gofiber/fiber/v2"
 	"github.com/turistikrota/service.shared/server/http/result"
@@ -21,19 +22,31 @@ func (h Server) FeatureUpdate(ctx *fiber.Ctx) error {
 }
 
 func (h Server) FeatureDelete(ctx *fiber.Ctx) error {
-	return nil
+	detail := dto.Request.FeatureDetail()
+	h.parseParams(ctx, detail)
+	_, err := h.app.Commands.FeatureDelete.Handle(ctx.UserContext(), detail.ToDeleteCommand())
+	return result.IfSuccess(err, ctx, h.i18n, Messages.Success.FeatureDelete)
 }
 
 func (h Server) FeatureDisable(ctx *fiber.Ctx) error {
-	return nil
+	detail := dto.Request.FeatureDetail()
+	h.parseParams(ctx, detail)
+	_, err := h.app.Commands.FeatureDisable.Handle(ctx.UserContext(), detail.ToDisableCommand())
+	return result.IfSuccess(err, ctx, h.i18n, Messages.Success.FeatureDisable)
 }
 
 func (h Server) FeatureEnable(ctx *fiber.Ctx) error {
-	return nil
+	detail := dto.Request.FeatureDetail()
+	h.parseParams(ctx, detail)
+	_, err := h.app.Commands.FeatureEnable.Handle(ctx.UserContext(), detail.ToEnableCommand())
+	return result.IfSuccess(err, ctx, h.i18n, Messages.Success.FeatureEnable)
 }
 
 func (h Server) FeatureList(ctx *fiber.Ctx) error {
-	return nil
+	res, err := h.app.Queries.FeatureListAll.Handle(ctx.UserContext(), query.FeatureListAllQuery{})
+	return result.IfSuccessDetail(err, ctx, h.i18n, Messages.Success.FeatureList, func() interface{} {
+		return dto.Response.FeatureList(res)
+	})
 }
 
 func (h Server) PlaceCreate(ctx *fiber.Ctx) error {
